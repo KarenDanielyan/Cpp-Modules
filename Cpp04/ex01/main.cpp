8dc991d120ad2a93b76a614d0c93133173bee629
KarenDanielyan <karendanielyan66@gmail.com>
2024-07-30 20:48:23 +0400

    Cpp04/ex01 complete

--- Cpp04/ex01/main.cpp
+++ Cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 15:39:18 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/07/30 20:25:06 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/07/30 20:47:23 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,21 @@
 #include "Cat.h"
 #include "Dog.h"
 
+void	atexit()
+{
+	system("leaks ex01");
+}
+
 int	main(void)
 {
-	Cat	*megatron;
-	Dog	*cupcake;
+	Animal*	animals[100];
 
-	megatron = new Cat();
-	cupcake = new Dog();
-
-	delete megatron;
-	delete cupcake;
+	std::atexit(&atexit);
+	for (int i = 0; i < 50; i++)
+		animals[i] = new Cat();
+	for (int i = 50; i < 100; i++)
+		animals[i] = new Dog();
+	for (int i = 0; i < 100; i ++)
+		delete animals[i];
 	return (0);
 }
