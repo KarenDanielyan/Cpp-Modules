2ced07a7ce02ff92052a8f2f65516525bac80377
KarenDanielyan <karendanielyan66@gmail.com>
2024-08-02 16:36:03 +0400

    Removing system(leaks) as it only works on mac

--- Cpp04/ex02/main.cpp
+++ Cpp04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 15:39:18 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/07/31 14:03:59 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/08/02 16:35:33 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,10 @@
 #include "Cat.h"
 #include "Dog.h"
 
-void	atexit()
-{
-	system("leaks ex02");
-}
-
 int	main(void)
 {
 	AAnimal*	animals[100];
 
-	std::atexit(&atexit);
 	for (int i = 0; i < 50; i++)
 		animals[i] = new Cat();
 	for (int i = 50; i < 100; i++)
