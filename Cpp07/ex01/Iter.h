ec797fd08b1708b883f511f730022594a9d24c2c
Karen Danielyan <karendanielyan66@gmail.com>
2024-08-30 18:38:58 +0400

    Fixed an issue, where iter could not work with non-const arrays

--- Cpp07/ex01/Iter.h
+++ Cpp07/ex01/Iter.h
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 02:47:34 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/08/22 03:19:52 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/08/30 18:38:18 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,16 @@
 # include <iostream>
 
 template <typename T>
-void	decorate(T& item)
+void	decorate(T const & item)
 {
 	std::cout << "Now [" << item << "] is decorated" << std::endl;
+}
+
+template <typename T>
+void	iter(T* array, size_t size, void (*func)(T &))
+{
+	for (size_t i = 0; i < size; i ++)
+		func(array[i]);
 }
 
 template <typename T>
