b3a5a1e378aa71c98c7026d9d1a82b7f42dec305
KarenDanielyan <karendanielyan66@gmail.com>
2024-09-16 18:17:47 +0400

    Modified validateDate to throw an error when date < 2009

--- Cpp09/ex00/Utils.cpp
+++ Cpp09/ex00/Utils.cpp
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 15:45:24 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/09/02 16:10:48 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/09/16 18:15:22 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ bool Utils::validateDate(std::string const & date)
 	year = std::atoi(date.substr(0, 4).c_str());
 	month = std::atoi(date.substr(5, 2).c_str());
 	day = std::atoi(date.substr(8, 2).c_str());
-	if (year < 1970 || month < 1 || month > 12 || day < 1 || day > 31)
+	if (year < 2009 || month < 1 || month > 12 || day < 1 || day > 31)
 		return (false);
 	return (true);
 }
